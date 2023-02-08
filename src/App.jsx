05df1c035e8eea246e44620d71afd88551552df8
Andres Melara <andres.melara.t@gmail.com>
2023-02-08 17:01:31 -0500

    Create the emoji range slider code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-const emojis = ["😞", "😐", "😃"];
+import { useState } from "react";
 
-function App() {}
+const emojis = ["😭", "😩", "😢", "😞", "😐", "😋", "😃", "😁", "😎"];
+
+function App() {
+  const [rangeValue, setRangeValue] = useState("4");
+  return (
+    <main className="flex h-screen flex-col items-center justify-center bg-space-cadet text-floral-white">
+      <label htmlFor="emoji-slider">How are you feeling?</label>
+      <input
+        type="range"
+        id="emoji-slider"
+        className="accent-tiffany-blue"
+        max="8"
+        value={rangeValue}
+        onChange={(e) => {
+          setRangeValue(e.target.value);
+        }}
+      />
+      <div className=" m-6 text-9xl">{emojis[rangeValue]}</div>
+    </main>
+  );
+}
 
 export default App;

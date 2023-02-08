2a93feb342607a75e4cf2c1be0f42297911969a6
Andres Melara <andres.melara.t@gmail.com>
2023-02-08 17:00:47 -0500

    Add colors to tailwinds config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.jsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "space-cadet": "#392F5A",
+        "tiffany-blue": "#9DD9D2",
+        "floral-white": "#FFF8F0",
+        jasmine: "#F4D06F",
+        "ut-orange": "#FF8811",
+      },
+    },
   },
   plugins: [],
 };
